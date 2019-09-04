60d2e8313e5251f54a00086f1a8049a2c6ea3716
riquitellez <54512082+riquitellez@users.noreply.github.com>
2019-09-04 22:32:15 +0200

    Add files via upload

---
+++ PruebaMVC.Datos/Entidades/Comprador.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PruebaMVC.Datos.Entidades
+{
+    public class Comprador
+    {
+        public Comprador(){}
+        public Comprador(string nombre, string primerApellido, string segundoApellido)
+        {
+            Id = Guid.NewGuid();
+            Nombre = nombre;
+            PrimerApellido = primerApellido;
+            SegundoApellido = segundoApellido;
+        }
+
+        [Key]
+        public Guid Id { get; protected set; }
+        [Required]
+        public string Nombre { get; protected set; }
+        [Required]
+        public string PrimerApellido { get; protected set; }
+        [Required]
+        public string SegundoApellido { get; protected set; }
+
+        public void ModificarDatos(string nombre, string primerApellido, string segundoApellido)
+        {
+            Nombre = nombre;
+            PrimerApellido = primerApellido;
+            SegundoApellido = segundoApellido;
+        }
+    }
+}
---
+++ PruebaMVC.Datos/Migrations/20190903203840_PrimeraMigracion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PruebaMVC.Datos.Migrations
+{
+    public partial class PrimeraMigracion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Compradores",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Nombre = table.Column<string>(nullable: false),
+                    PrimerApellido = table.Column<string>(nullable: false),
+                    SegundoApellido = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Compradores", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Compradores");
+        }
+    }
+}
