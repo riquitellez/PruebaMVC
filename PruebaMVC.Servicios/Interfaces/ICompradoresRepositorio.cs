355100ec666c9c7b022455ebdaef02871b5ffafd
riquitellez <54512082+riquitellez@users.noreply.github.com>
2019-09-04 22:33:03 +0200

    Add files via upload

---
+++ PruebaMVC.Servicios/Interfaces/ICompradoresRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using PruebaMVC.Dominio.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PruebaMVC.Servicios.Interfaces
+{
+    public interface ICompradoresRepositorio
+    {
+        void AnadirComprador(string nombre, string primerApellido, string segundoApellido);
+
+        void EditarComprador(Guid id, string nombre, string primerApellido, string segundoApellido);
+
+        void EliminarComprador(Guid id);
+
+        CompradorDetalleModel ObtenerDetallesComprador(Guid id);
+        IEnumerable<CompradorParaListadoModel> ObtenerListado(string busqueda, int numPagina, int tamanoPagina);
+        int CountCompradores(string busqueda);
+    }
+}
